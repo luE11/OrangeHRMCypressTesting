33ab98969934e73970bd341f2519e4d92f517f38
Yohan Caro <yohancaro7@gmail.com>
2022-08-11 12:37:10 -0500

    Chech data finish
    Siiuuuu

--- cypress/e2e/test_form.cy.js
+++ cypress/e2e/test_form.cy.js
@@ -2,6 +2,15 @@
 
 describe('Casos de prueba para el registro de usuarios', function() {
 
+    const boss = 'John Smith'
+    var users;
+
+    before(() => {
+        cy.request('GET', 'https://ldatagenerator.herokuapp.com/3').then((res) => {
+            users = res.body
+        })
+    })
+
     beforeEach(() => {
         cy.visit("https://opensource-demo.orangehrmlive.com/index.php/admin/viewSystemUsers")
 
@@ -19,23 +28,52 @@ describe('Casos de prueba para el registro de usuarios', function() {
         cy.get('#menu_admin_viewSystemUsers').click({ force: true })
         cy.get('#btnAdd').click()
 
-        cy.request('GET', 'https://ldatagenerator.herokuapp.com/').then((res) => {
-            let users = res.body
-            const boss = 'John Smith'
+        users.forEach(element => {
+            console.log(element)
+            cy.get('#systemUser_userType').select(element.user_role)
+            cy.get('#systemUser_employeeName_empName').type(boss)
+            cy.get('#systemUser_userName').type(element.username)
+            cy.get('#systemUser_status').select(element.status)
+            cy.get('#systemUser_password').type(element.password)
+            cy.get('#systemUser_confirmPassword').type(element.password)
+            cy.get('#btnSave').click()
 
-            users.forEach(element => {
-                console.log(element)
-                cy.get('#systemUser_userType').select(element.user_role)
-                cy.get('#systemUser_employeeName_empName').type(boss)
-                cy.get('#systemUser_userName').type(element.username)
-                cy.get('#systemUser_status').select(element.status)
-                cy.get('#systemUser_password').type(element.password)
-                cy.get('#systemUser_confirmPassword').type(element.password)
-                cy.get('#btnSave').click()
-
-                cy.get('#btnAdd').click()
-            });
+            cy.get('#btnAdd').click()
         })
+        cy.get('#btnCancel').click()
+    })
+
+    //Caso de prueba #2 - verificación
+    it('Should be to verify that the users were created', function() {
+        users.forEach(element => {
+            cy.get('#searchSystemUser_userName').type(element.username)
+            cy.get('#searchSystemUser_userType').select(element.user_role)
+            cy.get('#searchSystemUser_employeeName_empName').type(boss)
+            cy.get('#searchSystemUser_status').select(element.status)
+
+            cy.get('#searchBtn').click()
+
+            cy.get('#resultTable > tbody').children().first().children().as('columns').each(($el, index, $list) => {
+                switch (index) {
+                    case 1:
+                        cy.get('@columns').eq(index).contains(element.username)
+                        break;
+                    case 2:
+                        cy.get('@columns').eq(index).contains(element.user_role)
+                        break;
+                    case 3:
+                        cy.get('@columns').eq(index).contains(boss)
+                        break;
+                    case 4:
+                        cy.get('@columns').eq(index).contains(element.status)
+                        break;
+                    default:
+                        break;
+                }
+            })
+            cy.get('#resetBtn').click()
+        })
+
     })
 
 })
